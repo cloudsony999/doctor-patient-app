048a8a687a909977b59d639f748dd22ac90aaf60
cloudsony999 <cloudsony999@gmail.com>
2022-05-29 22:45:54 +0530

    c

--- src/app/service/fetch-data-from-json.service.ts
+++ src/app/service/fetch-data-from-json.service.ts
@@ -8,8 +8,10 @@ export class FetchDataFromJSONService {
 
   constructor(private http: HttpClient) { }
 
-  url="./../../assets/doctor-patient-list.json";
+  //url="./../../assets/doctor-patient-list.json";
+  //url="./../../assets/target.json";
+  private url1="http://localhost:8888/api/getAllDoctors";
   getData() {
-    return this.http.get(this.url);
+    return this.http.get(this.url1);
   }
 }
